71a52729bc58a92ed2e7d9c0ba4dad15689db834
zimv <472953586@qq.com>
2020-02-22 17:40:42 +0800

    chore: build config

--- jest.config.js
+++ jest.config.js
@@ -14,11 +14,6 @@ module.exports = {
             }
           }
         ],
-      ],
-      'plugins': [
-        '@babel/plugin-transform-runtime',
-        '@babel/plugin-proposal-class-properties',
-        '@babel/plugin-proposal-object-rest-spread',
       ]
     }]
   }
